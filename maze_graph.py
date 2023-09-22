cf50778a59de31388ee4a66729c905aa460bb49d
hlin863 <zcabhli@ucl.ac.uk>
2023-09-22 22:25:11 +0100

    Update maze_graph.py 1
    Presented a visualization of the graph structure.

--- maze_graph.py
+++ maze_graph.py
@@ -1,53 +1,45 @@
 import pygame
 
-# Represent the graph in the code
+# Represent the graph in the code with node positions
 graph = {
-    'A': ['B'],
-    'B': ['C', 'E'],
-    'C': ['B', 'D', 'G'],
-    'D': ['C'],
-    'E': ['B', 'F', 'G'],
-    'F': ['E', 'G', 'H'],
-    'G': ['C', 'E', 'F'],
-    'H': ['F', 'I', 'J'],
-    'I': ['H'],
-    'J': ['H', 'K', 'L'],
-    'K': ['J'],
-    'L': ['J', 'M', 'N', 'O'],
-    'M': ['L'],
-    'N': ['L'],
-    'O': ['L']
+    'A': {'connections': ['B'], 'position': (0, 0)},
+    'B': {'connections': ['A', 'C', 'E'], 'position': (1, 0)},
+    'C': {'connections': ['B', 'D', 'G'], 'position': (2, 0)},
+    'D': {'connections': ['C'], 'position': (3, 0)},
+    'E': {'connections': ['B', 'F', 'G'], 'position': (1, 1)},
+    'F': {'connections': ['E', 'G', 'H'], 'position': (1, 2)},
+    'G': {'connections': ['C', 'E', 'F'], 'position': (2, 1)},
+    'H': {'connections': ['F', 'I', 'J'], 'position': (1, 3)},
+    'I': {'connections': ['H'], 'position': (0, 3)},
+    'J': {'connections': ['H', 'K', 'L'], 'position': (2, 3)},
+    'K': {'connections': ['J'], 'position': (3, 3)},
+    'L': {'connections': ['J', 'M', 'N', 'O'], 'position': (2, 4)},
+    'M': {'connections': ['L'], 'position': (1, 4)},
+    'N': {'connections': ['L'], 'position': (3, 4)},
+    'O': {'connections': ['L'], 'position': (2, 5)}
 }
 
-# Define the start and goal nodes
 start = 'A'
 goal = 'O'
 
-# Define a function to perform DFS
+# DFS as before
 
 
 def dfs(graph, node, visited):
     if node not in visited:
         visited.append(node)
-        for neighbour in graph[node]:
+        for neighbour in graph[node]['connections']:
             dfs(graph, neighbour, visited)
     return visited
 
 
-# Find a path between start and goal using DFS
 visited_nodes = dfs(graph, start, [])
 
-# Check if the goal node was visited
-if goal in visited_nodes:
-    print("Path found!")
-else:
-    print("No path available")
-
-# Pygame visualization setup
+# Pygame setup
 pygame.init()
 CELL_SIZE = 40
-SCREEN_WIDTH = CELL_SIZE * len(graph)
-SCREEN_HEIGHT = CELL_SIZE
+SCREEN_WIDTH = CELL_SIZE * 5  # Assuming 5x5 maze for simplicity
+SCREEN_HEIGHT = CELL_SIZE * 6
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Graph Traversal')
 
@@ -58,21 +50,29 @@ COLORS = {
     'goal': (255, 255, 0)
 }
 
-# Draw the graph and visualize the traversal
-
 
 def draw_graph():
     screen.fill((255, 255, 255))
 
-    for index, node in enumerate(graph):
+    # Draw connections first
+    for node, data in graph.items():
+        x1, y1 = data['position']
+        for connection in data['connections']:
+            x2, y2 = graph[connection]['position']
+            pygame.draw.line(screen, (0, 0, 0), (x1 * CELL_SIZE + CELL_SIZE // 2, y1 * CELL_SIZE +
+                             CELL_SIZE // 2), (x2 * CELL_SIZE + CELL_SIZE // 2, y2 * CELL_SIZE + CELL_SIZE // 2))
+
+    # Draw nodes
+    for node, data in graph.items():
         color = COLORS['visited'] if node in visited_nodes else COLORS['node']
         if node == start:
             color = COLORS['start']
         elif node == goal:
             color = COLORS['goal']
 
-        pygame.draw.circle(screen, color, (index * CELL_SIZE +
-                           CELL_SIZE // 2, SCREEN_HEIGHT // 2), 15)
+        x, y = data['position']
+        pygame.draw.circle(screen, color, (x * CELL_SIZE +
+                           CELL_SIZE // 2, y * CELL_SIZE + CELL_SIZE // 2), 15)
 
     pygame.display.flip()
 
